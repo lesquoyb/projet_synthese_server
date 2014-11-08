3b7ae3975188a640c8816cec72924016e64fa840
Baptiste Lesquoy <nouvelleaddresse@yahoo.fr>
2014-11-08 17:27:33 +0100

    nettoyage + élimination de l'erreur lors de la déconnexion d'un utilisateur

--- server/src/server/Main.java
+++ server/src/server/Main.java
@@ -7,41 +7,31 @@ import java.net.Socket;
 public class Main {
 
 	public static void main(String[] args)throws Exception{
-	ServerSocket serveur;
-	serveur = new ServerSocket(9111); // place un serveur à l'écoute sur le port 9111
-	                     //si le paramètre = 0 alors la JVM choisit un port libre
-
-	System.out.println("serveur de majuscule démarré : "+serveur);
-
-	InetAddress cetteMachine;
-	int portLocal;
-	ThreadGroup groupe;
-
-	portLocal = serveur.getLocalPort();
-
-	cetteMachine = InetAddress.getLocalHost();
-
-	System.out.println("adresse IP du serveur de majuscule : "+cetteMachine.getHostAddress());
-	System.out.println("port du serveur : "+portLocal);
-
-	groupe = new ThreadGroup("socketsClients");
-	int noConnexion = 0;
-	while(true) //attente infinie du serveur sur le port
-	    {
-	    Socket nouveauClientSocket;
-	    ReceveurEnvoyeur nouveauClientThread;
-	    
-	    nouveauClientSocket = serveur.accept(); // attente de connexion de la part d'un nouveau client
-	    ++noConnexion; // la connexion a eu lieu et un socket a été créé : nouveauClientSocket
-	    System.out.println("Connexion réussie n° : "+noConnexion);
-	    
-	    /* à présent création d'un thread pour gérer les transactions avec le nouvau client en parallèle 
-	     * avec les autres clients déjà connectés et avec l'attente perpétuelle du servur*/
-	    
-	    nouveauClientThread = new ReceveurEnvoyeur(nouveauClientSocket, groupe, noConnexion); 
-	    nouveauClientThread.start();
-	    }
-	} // main
+		
+		ServerSocket serveur = new ServerSocket(0); // place un serveur à l'écoute sur le port passé, si 0, alors choix parmi les ports libre
+	
+		System.out.println("serveur démarré: "+serveur);
+	
+		InetAddress cetteMachine = InetAddress.getLocalHost();
+	
+		System.out.println("adresse IP du serveur: "+cetteMachine.getHostAddress());
+		System.out.println("port du serveur : "+ serveur.getLocalPort());
+	
+		ThreadGroup groupe = new ThreadGroup("socketsClients");
+		int noConnexion = 0;
+		while(true){
+		    
+			Socket nouveauClientSocket = serveur.accept(); // attente de connexion de la part d'un nouveau client
+		    ++noConnexion; // la connexion a eu lieu et un socket a été créé : nouveauClientSocket
+		    System.out.println("Connexion réussie n°: "+noConnexion);
+		    
+		    /* à présent création d'un thread pour gérer les transactions avec le nouvau client en parallèle 
+		     * avec les autres clients déjà connectés et avec l'attente perpétuelle du servur*/
+		    
+		    ReceveurEnvoyeur nouveauClientThread = new ReceveurEnvoyeur(nouveauClientSocket, groupe, noConnexion); 
+		    nouveauClientThread.start();
+		}
+	} 
 
 
 }
--- server/src/server/ReceveurEnvoyeur.java
+++ server/src/server/ReceveurEnvoyeur.java
@@ -1,53 +1,59 @@
 package server;
 import java.net.*;
 import java.io.*;
-/**
- * thread et socket coté serveur :  traite les requêtes d'un client
- * ici le serveur, pour chaque chaîne de caractères reçue d'un client, la transforme en majuscule puis la renvoie au client
-* */
-public class ReceveurEnvoyeur extends Thread
-{
-Socket socket;  int noConnexion; // numéro du client distant
 
-BufferedReader fluxEntrant;	PrintStream fluxSortant;
 
 /**
-* Suppose socket déjà connecté vers le client n° noConnexion
- * @param noConnexion : n° du client
- * */
-public ReceveurEnvoyeur(Socket socket, ThreadGroup groupe, int noConnexion) throws IOException
-{
-super(groupe,"ReceveurEnvoyeur");
-this.socket = socket;
-this.noConnexion = noConnexion;
-
-fluxEntrant = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-/* à présent fluxEntrant est prêt pour lire du texte provenant du client */
-
-fluxSortant = new PrintStream(this.socket.getOutputStream());
-/* à présent fluxSortant est prêt pour renvoyer des réponses textuelles au client */
-}
-
-public void run()
-{
-String ligne;	String réponse;
-
-try
-{
- while (!isInterrupted())
-     {
-     ligne = fluxEntrant.readLine(); // saisit le texte du client
-     System.out.println(" le client n° "+this.noConnexion+" a envoyé : ");
-     System.out.println(ligne); // écho de la question sur la console
-     ligne = ligne.trim();
-     réponse = ligne.toUpperCase(); // calcul de la réponse
-     
-     fluxSortant.print(réponse+'\n'); // envoi de la reponse au client
-     sleep(5);
-     } //while
-}
-catch(InterruptedException erreur) { /* le thread s'arrête */}
-catch(IOException erreur) { System.err.println(" on ne peut pas lire sur le socket provenant du client");}
- 
-}// run
-} // ReceveurEnvoyeur
+ * Serveur dont le but est d'afficher grâce à awt les formes géométriques transmisent par le client.
+ * Il doit respecter le pattern chain of responsibility
+ * @author baptiste
+ *
+ */
+public class ReceveurEnvoyeur extends Thread{
+		
+	Socket socket;  
+	int noConnexion; // numéro du client distant
+	BufferedReader fluxEntrant;	
+	PrintStream fluxSortant;
+	
+	/**
+	* Suppose socket déjà connecté vers le client n° noConnexion
+	* @param noConnexion : n° du client
+	**/
+	public ReceveurEnvoyeur(Socket socket, ThreadGroup groupe, int noConnexion) throws IOException{
+			
+		super(groupe,"ReceveurEnvoyeur");
+		this.socket = socket;
+		this.noConnexion = noConnexion;
+		
+		fluxEntrant = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		/* à présent fluxEntrant est prêt pour lire du texte provenant du client */
+		
+		fluxSortant = new PrintStream(this.socket.getOutputStream());
+		/* à présent fluxSortant est prêt pour renvoyer des réponses textuelles au client */
+	}
+	
+	public void run(){
+			
+		String ligne, réponse;
+		
+		try{
+			 while ( ! isInterrupted() ){
+				 
+			     ligne = fluxEntrant.readLine(); 
+			     if(ligne != null){
+				     System.out.println(" le client n° "+this.noConnexion+" a envoyé : ");
+				     System.out.println(ligne); 
+				     ligne = ligne.trim();
+				     réponse = ligne.toUpperCase();
+				     
+				     fluxSortant.print(réponse+'\n'); 
+				     sleep(5);
+			     }
+			}
+		}
+		catch(InterruptedException erreur) { /* le thread s'arrête */}
+		catch(IOException erreur) { System.err.println(" on ne peut pas lire sur le socket provenant du client");}
+		 
+	}
+} 
